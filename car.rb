b346f7a855afec0dd83c1a2b6b2b79a59fd65e5b
Diana Man <dianaman82@gmail.com>
2016-10-03 16:30:32 +0100

    created car class and car spec

---
+++ car.rb
@@ -0,0 +1,8 @@
+class Car
+
+  def initialize(fuel_level, speed)
+    @fuel_level = fuel_level
+    @speed = speed
+  end
+    
+end
---
+++ spec/car_spec.rb
@@ -0,0 +1,17 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../car')
+
+class TestCar < Minitest::Test
+
+def setup
+  @car1 = Car.new("fuel_level", "speed")
+end
+
+def test
+
+
+
+
+
+end
